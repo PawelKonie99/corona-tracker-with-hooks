7990642efc5efa67477b0a4d725a001f12cb51b7
PawelKonie99 <PawelKonie99@users.noreply.github.com>
2020-07-04 23:05:20 +0200

    Updates

---
+++ precache-manifest.b18fa0977923385a2abef6b6e786affa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4be7820f9c8438f60a2bf6d4caddd8d",
+    "url": "/corona-tracker-with-hooks/index.html"
+  },
+  {
+    "revision": "3e58fa14814c43a26ffd",
+    "url": "/corona-tracker-with-hooks/static/css/main.991e8d77.chunk.css"
+  },
+  {
+    "revision": "354e7e56c7926349dd08",
+    "url": "/corona-tracker-with-hooks/static/js/2.4f2dee55.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/corona-tracker-with-hooks/static/js/2.4f2dee55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e58fa14814c43a26ffd",
+    "url": "/corona-tracker-with-hooks/static/js/main.9b2d4c7a.chunk.js"
+  },
+  {
+    "revision": "6ad17a29c90f96d2275f",
+    "url": "/corona-tracker-with-hooks/static/js/runtime-main.2c92b85e.js"
+  }
+]);
